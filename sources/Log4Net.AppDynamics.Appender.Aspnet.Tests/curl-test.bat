7f79d5806413d6ec0b5d4f5aef880f72a42a7a29
Charles Lin <charles.lin@appdynamics.com>
2020-12-02 16:06:27 -0700

    Put 1 second delays and increased loop count for test.

--- sources/Log4Net.AppDynamics.Appender.Aspnet.Tests/curl-test.bat
+++ sources/Log4Net.AppDynamics.Appender.Aspnet.Tests/curl-test.bat
@@ -1,13 +1,12 @@
-echo Test script to send 1000 requests
+echo Test script to send 1000000 requests
 @echo off
 
-set countfiles=1000
+set countfiles=1000000
 
 :loop
 
 set /a countfiles -= 1
 echo %countfiles% 
 curl -k https://localhost:5001
-
+PING localhost -n 2 >NUL
 if %countfiles% GTR 0 goto loop
-
